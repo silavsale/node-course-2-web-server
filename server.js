4dccf873bf567e575ffb0b6aa10c59d8b281cd23
Silavsale <silavsale@gmail.com>
2019-01-12 14:06:08 -0500

    Add project page

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-const port = process.env.PORT || 3000 ;
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -53,6 +53,13 @@ app.get('/bad', (req, res) => {
     res.send({
         name: 'ERROR',
         text: 'you have an error'
+    });
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects page',
+        text: 'My projects.'
     });
 });
 
